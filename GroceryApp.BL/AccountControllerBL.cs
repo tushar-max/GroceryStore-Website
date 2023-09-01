3e3bf68d20436693b0c00433ee10093ebeb6e1e1
Tushar Awasthi <tusharsaumyaab@gmail.com>
2023-09-01 16:33:26 +0530

    Uploading The Business Logic Layer

---
+++ GroceryApp.BL/AccountControllerBL.cs
@@ -0,0 +1,57 @@
+﻿using GroceryApp.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryApp.BL
+{
+    public class AccountControllerBL
+    {
+        private readonly GroceryAppDbContext _groceryAppDbContext;
+
+        public AccountControllerBL(GroceryAppDbContext groceryAppDbContext)
+        {
+            _groceryAppDbContext = groceryAppDbContext;
+        }
+
+        public async Task<bool> RegisterUserBL(RegisterUser registerUser)
+        {
+            var duplicateUsername = await _groceryAppDbContext.registerUsers.Where(x => x.Email == registerUser.Email).FirstOrDefaultAsync();
+            if (duplicateUsername == null)
+            {
+                await _groceryAppDbContext.registerUsers.AddAsync(registerUser);
+                await _groceryAppDbContext.SaveChangesAsync();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<LoginStatus> LoginUserBL(Login loginUser)
+        {
+            var validateLogin = await _groceryAppDbContext.registerUsers.
+                Where(x => x.Email == loginUser.Email && x.Password == loginUser.Password).FirstOrDefaultAsync();
+            LoginStatus loginStatus = new LoginStatus();
+            if (validateLogin == null)
+            {
+                loginStatus.UserName = "";
+                loginStatus.Email = "";
+                loginStatus.LoggedIn = false;
+                loginStatus.IsAdmin = false;
+            }
+            else
+            {
+                loginStatus.UserName = validateLogin.UserName;
+                loginStatus.Email = validateLogin.Email;
+                loginStatus.LoggedIn = true;
+                loginStatus.IsAdmin = validateLogin.IsAdmin;
+            }
+            return loginStatus;
+        }
+    }
+}
---
+++ GroceryApp.BL/ProductControllerBL.cs
@@ -0,0 +1,63 @@
+﻿using GroceryApp.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryApp.BL
+{
+    public class ProductControllerBL
+    {
+        private readonly GroceryAppDbContext _groceryAppDbContext;
+
+        public ProductControllerBL(GroceryAppDbContext context)
+        {
+            _groceryAppDbContext = context;
+        }
+
+        public List<CartProducts> GetAllCartItemsBL()
+        {
+            return _groceryAppDbContext.cartProducts.ToList();
+        }
+
+        public Product GetCartProductsBL(string id)
+        {
+            return _groceryAppDbContext.products.Where(x => x.Id.ToString() == id).FirstOrDefault();
+        }
+        public async Task<List<OrdersPlaced>> GetOrdersBL(string email)
+        {
+            return await _groceryAppDbContext.ordersPlaceds.Where(x => x.email == email).ToListAsync();
+        }
+        public async Task<Product> EditRecordBL(Product product)
+        {
+            var data = await _groceryAppDbContext.products.SingleOrDefaultAsync(x => x.Id.ToString() == product.Id.ToString());
+            data.ProductName = product.ProductName;
+            data.Description = product.Description;
+            data.Price = product.Price;
+            data.Image = product.Image;
+            _groceryAppDbContext.Update(data);
+            await _groceryAppDbContext.SaveChangesAsync();
+            return data;
+        }
+        public async Task<Product> DeleteRecordBL(string id)
+        {
+            var data = await _groceryAppDbContext.products.SingleOrDefaultAsync(x => x.Id.ToString() == id);
+            _groceryAppDbContext.products.Remove(data);
+            await _groceryAppDbContext.SaveChangesAsync();
+            return data;
+        }
+        public async Task<List<Product>> GetAllProductsBL()
+        {
+            var productsList = await _groceryAppDbContext.products.ToListAsync();
+            return productsList;
+        }
+        public async Task<List<Product>> GetCategoryProductBL(int categoryId)
+        {
+            var productList = await _groceryAppDbContext.products.Where(x => x.Category == categoryId).ToListAsync();
+            return productList;
+        }
+    }
+}
